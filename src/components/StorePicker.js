90e7b7cb06c8fb562b805ef41d07c96cf2e3ec18
Salaj Mithbawkar <salaj.mithbawkar@cgx.com.au>
2018-02-26 16:03:50 +1100

    Get dynamic store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	// constructor() {
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+	goToStore(event) {
+		event.preventDefault();
+		console.log('You changed the URL');
+		//grabbing the text from the box
+		console.log(this.storeInput.value);
+		//transitioning to respective routes
+
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
 				<h2>Please enter a store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+				<input type="text" required placeholder="Store Name" defaultValue={
+					getFunName()} ref={(input) => { this.storeInput = input}} />
 				<button type="submit">Visit Store -></button>
 			</form>
 		)
